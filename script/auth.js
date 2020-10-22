e827e09ef7c6e939be662b3053640e37fc198ec5
Mai Duy Dũng <maiduydung@yahoo.com>
2020-10-23 01:18:29 +0900

    added logout method

--- script/auth.js
+++ script/auth.js
@@ -1,2 +1,28 @@
 //sign up
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    //get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
+    const name = signupForm['signup-name'].value;
 
+    console.log(email, password, name);
+
+    // register user
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred);
+        //after successful registeration, switch to index page
+        window.location.href = 'index.html';
+    });
+});
+
+
+//logout
+function userLogOut(){
+    auth.signOut().then(() =>{
+        console.log('user signed out\n');
+        window.location.href = 'fresh.html';
+    
+    })
+};
