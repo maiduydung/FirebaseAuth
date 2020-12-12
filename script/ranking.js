5a02dd97f3b0b91f126d4f30a715a9cb9dd09fa0
Mai Duy Dũng <maiduydung@yahoo.com>
2020-12-12 23:03:54 +0900

    wait 2 secs to get data

--- script/ranking.js
+++ script/ranking.js
@@ -7,7 +7,13 @@ db_remote.ref("Home_Appliances_Control/Air_Con/Fragment").once("value").then((sn
     console.log("parsed",Date.parse(snapshot.val().start));
 });
 
-var end = Date.parse(new Date());
-var hour = (end - start)*2.77777777*0.00000001;
-console.log("hour ", hour);
 
+
+
+function foo(){
+    var end = Date.parse(new Date());
+    var hour = (end - start)*2.77777777*0.00000001;
+    console.log("hour ", hour);
+}
+
+setTimeout(foo, 2000);
