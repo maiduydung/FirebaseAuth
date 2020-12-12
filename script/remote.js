cf92dca4a16321b207025def3f23c1275a3b86a1
Mai Duy Dũng <maiduydung@yahoo.com>
2020-12-12 23:51:33 +0900

    write start time to a different dataframe

--- script/remote.js
+++ script/remote.js
@@ -41,6 +41,34 @@ getUserID();
 //     console.log(snapshot.val().start);
 // })
 
+function writeAirConStartTime(){
+    var startTime = db_remote.ref("Energy_Consumption/users/"+uid);
+    startTime.update({
+        air_con: start(),
+    })
+}
+
+function writeFanStartTime(){
+    var startTime = db_remote.ref("Energy_Consumption/users/"+uid);
+    startTime.update({
+        fan: start(),
+    })
+}
+
+function writeTVStartTime(){
+    var startTime = db_remote.ref("Energy_Consumption/users/"+uid);
+    startTime.update({
+        tv: start(),
+    })
+}
+
+function writeLightStartTime(){
+    var startTime = db_remote.ref("Energy_Consumption/users/"+uid);
+    startTime.update({
+        light: start(),
+    })
+}
+
 
 function aircon_on(){
     var aircon_ref_frag = db_remote.ref("Home_Appliances_Control/Air_Con/Fragment");
@@ -48,8 +76,8 @@ function aircon_on(){
     if(aircon_switch.checked == true){
         aircon_ref_frag.update({
             key:1,
-            start : start(),
         })
+        writeAirConStartTime();
         console.log("turned on air conditioner\n");
     }
     else{
@@ -79,9 +107,9 @@ function fan_switch(){
     var fan_switch = document.getElementById("fan_switch");
     if(fan_switch.checked == true){
         fan_ref_frag.update({
-            start : start(),
             key:1,
         })
+        writeFanStartTime();
         console.log("turned on fan\n");
     }
     else{
@@ -98,9 +126,9 @@ function tv_switch(){
     var tv_switch = document.getElementById("tv_switch");
     if(tv_switch.checked == true){
         tv_ref_frag.update({
-            start : start(),
             key:0,
         })
+        writeTVStartTime();
         console.log("turned on tv\n");
     }
     else{
@@ -118,9 +146,9 @@ function light_switch(){
     var light_switch = document.getElementById("light_switch");
     if(light_switch.checked == true){
         light_ref_frag.update({
-            start : start(),
             key:1,
         })
+        writeLightStartTime();
         console.log("turned on light\n");
     }
     else{
