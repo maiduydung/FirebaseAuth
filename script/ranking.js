7d3738c1286fa6268514a96b8f128388c400dcd3
Mai Duy Dũng <maiduydung@yahoo.com>
2020-12-12 23:22:20 +0900

    added get_aircon_kWh

--- script/ranking.js
+++ script/ranking.js
@@ -1,19 +1,22 @@
 // GET START TIME SAMPLE
 
-var start;
-db_remote.ref("Home_Appliances_Control/Air_Con/Fragment").once("value").then((snapshot) =>{
-    console.log(snapshot.val().start);
-    start = Date.parse(snapshot.val().start)
-    console.log("parsed",Date.parse(snapshot.val().start));
-});
+function get_aircon_kWh(){
+    var start;
+    db_remote.ref("Home_Appliances_Control/Air_Con/Fragment").once("value").then((snapshot) =>{
+        start = Date.parse(snapshot.val().start)
+    });
 
-
-
-
-function foo(){
-    var end = Date.parse(new Date());
-    var hour = (end - start)*2.77777777*0.00000001;
-    console.log("hour ", hour);
+    var hour;
+    var energy;
+    function getHoursAndEnergy(){
+        var end = Date.parse(new Date());
+        hour = (end - start)*2.77777777*0.00000001;
+        console.log("hours ", hour);
+        energy = 850*hour/1000;
+        console.log('aircon kwh ', energy);
+    }
+    setTimeout(getHoursAndEnergy, 3000);
+    return energy;
 }
 
-setTimeout(foo, 2000);
+get_aircon_kWh();
